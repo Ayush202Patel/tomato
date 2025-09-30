bfc4da819a892cb374e4fab3af9570df6f8e430b
Ayush202Patel <ayushpatel2005.2@gmail.com>
2025-09-30 09:22:51 +0530

    tried adding routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-
+import { Routes } from "react-router-dom";
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Routes></Routes>
     </div>
   );
 };
