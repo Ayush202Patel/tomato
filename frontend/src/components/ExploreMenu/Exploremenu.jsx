729fde50b6cf9d5f52629def0ac83b25db727761
Ayush202Patel <ayushpatel2005.2@gmail.com>
2025-10-03 20:15:54 +0530

    Started menu

---
+++ frontend/src/components/ExploreMenu/Exploremenu.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./ExploreMenu.css";
+<ExploreMenu className="css"></ExploreMenu>;
+const Exploremenu = () => {
+  return (
+    <div className="explore-menu" id="exploree-menu">
+      <h1>Explore our menu</h1>
+      <p className="explore-menu-test">
+        Choose from a diverse menu featuring a delectable array of dishes
+        crafted with the finest ingrideints and culinary expertise.Our mission
+        is to satisfy your carvings and elevate your dining experience,one
+        delecious meal at a time
+      </p>
+      <div className="explore-menu-list"></div>
+    </div>
+  );
+};
+
+export default Exploremenu;
