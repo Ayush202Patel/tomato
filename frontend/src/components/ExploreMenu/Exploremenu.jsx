e5452f011e497a99cb15fd0f14879d09b48d14f8
Ayush202Patel <ayushpatel2005.2@gmail.com>
2025-10-04 23:32:33 +0530

    Added menu

--- frontend/src/components/ExploreMenu/Exploremenu.jsx
+++ frontend/src/components/ExploreMenu/Exploremenu.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 import "./ExploreMenu.css";
-<ExploreMenu className="css"></ExploreMenu>;
-const Exploremenu = () => {
+import { menu_list } from "../../assets/assets.js";
+const ExploreMenu = () => {
   return (
     <div className="explore-menu" id="exploree-menu">
       <h1>Explore our menu</h1>
-      <p className="explore-menu-test">
+      <p className="explore-menu-text">
         Choose from a diverse menu featuring a delectable array of dishes
         crafted with the finest ingrideints and culinary expertise.Our mission
         is to satisfy your carvings and elevate your dining experience,one
         delecious meal at a time
       </p>
-      <div className="explore-menu-list"></div>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div key={index} className="explore-menu-list-item">
+              <img src={item.menu_image} alt="" />
+              <p>{item.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
 
-export default Exploremenu;
+export default ExploreMenu;
