abc4f3c141ed24f78dae8abb2d16cd9a0e16b807
Ayush202Patel <ayushpatel2005.2@gmail.com>
2025-10-15 06:52:10 +0530

    Started Footere

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,20 @@ import { Route, Routes } from "react-router-dom";
 import Cart from "./pages/Cart/Cart";
 import Home from "./pages/Home/home";
 import PlaceOrder from "./pages/Placeorder/PlaceOrder";
+import Footer from "./components/Footer/Footer";
 const App = () => {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/order" element={<PlaceOrder />} />
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 };
 
