920dcbc5d2999e9e997020f046a34ea1ea647f5e
Ayush202Patel <ayushpatel2005.2@gmail.com>
2025-10-16 08:42:58 +0530

    Started styling footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { assets } from "../../assets/assets";
 const Footer = () => {
   return (
     <div id="footer" className="footer">
-      <div className="footer content">
+      <div className="footer-content">
         <div className="footer-content-left">
           <img src={assets.logo} alt="" />
           <p>
@@ -19,9 +19,27 @@ const Footer = () => {
             <img src={assets.linkedin_icon} alt="" />
           </div>
         </div>
-        <div className="footer-content-center"></div>
-        <div className="footer-content-right"></div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Deilvery</li>
+            <li>Privacy Policy </li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-212-456-7890</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
       </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 © Tomato.com-All Rights Reserved.
+      </p>
     </div>
   );
 };
