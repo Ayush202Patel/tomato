03d3f505c39082c0181ab1095fd65d0f28e1ee7c
Ayush Patel <ayushpatel2005.2@gmail.com>
2025-10-20 08:22:47 +0530

    Completed responsive

--- frontend/src/components/ExploreMenu/Exploremenu.jsx
+++ frontend/src/components/ExploreMenu/Exploremenu.jsx
@@ -3,7 +3,7 @@ import "./ExploreMenu.css";
 import { menu_list } from "../../assets/assets.js";
 const ExploreMenu = ({ category, setCategory }) => {
   return (
-    <div className="explore-menu" id="exploree-menu">
+    <div className="explore-menu" id="explore-menu">
       <h1>Explore our menu</h1>
       <p className="explore-menu-text">
         Choose from a diverse menu featuring a delectable array of dishes
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,28 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li className={menu === "home" ? "active" : ""}>home</li>
-        <li className={menu === "menu" ? "active" : ""}>menu</li>
-        <li className={menu === "mobile-app" ? "active" : ""}>mobile-app</li>
-        <li className={menu === "contact-us" ? "active " : ""}>contact us</li>
+        <Link to="/" className={menu === "home" ? "active" : ""}>
+          home
+        </Link>
+        <a href="#explore-menu" className={menu === "menu" ? "active" : ""}>
+          menu
+        </a>
+        <a
+          href="#app-download"
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </a>
+        <a href="#footer" className={menu === "contact-us" ? "active " : ""}>
+          contact us
+        </a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
