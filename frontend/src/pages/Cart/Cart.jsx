a408ef8cbc946b2534bd63e48f2eedd7727b9e29
Ayush Patel <ayushpatel2005.2@gmail.com>
2025-12-06 23:59:14 +0530

    Added logic

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -2,7 +2,8 @@ import React, { useContext } from "react";
 import "./Cart.css";
 import { StoreContext } from "../../context/StoreContext";
 const Cart = () => {
-  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } =
+    useContext(StoreContext);
   return (
     <div className="cart">
       <div className="cart-items">
@@ -42,7 +43,7 @@ const Cart = () => {
           <div>
             <div className="cart-total-details">
               <p>Subtotal</p>
-              <p>{0}</p>
+              <p>{getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cart-total-details">
@@ -52,7 +53,7 @@ const Cart = () => {
             <hr />
             <div className="cart-total-details">
               <b>Totals</b>
-              <b>{0}</b>
+              <b>{getTotalCartAmount() + 2}</b>
             </div>
           </div>
           <button>PROCEED TO CHECKOUT</button>
